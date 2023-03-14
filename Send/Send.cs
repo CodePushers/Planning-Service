70f6f9c57b857944cb493f7b80d4c64b4c0cf81b
Jacob Kaae <jacobkaae0511@gmail.com>
2023-03-14 13:57:37 +0100

    Uploaded en masse

---
+++ Send/Send.cs
@@ -0,0 +1,32 @@
+﻿using System.Numerics;
+using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+using shared;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "farvel",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+var planDTO = new PlanDTO { KundeNavn = "Anders", SlutSted = "Trapkasgade", StartSted = "Hos Rikke", StartTidspunkt = DateTime.Parse("2019-08-01") };
+
+string message = JsonSerializer.Serialize(planDTO);
+
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: string.Empty,
+                     routingKey: "farvel",
+                     basicProperties: null,
+                     body: body);
+
+
+Console.WriteLine($"Plan sendt:\n Kundenavn: {planDTO.KundeNavn}\nStarttidspunkt: {planDTO.StartTidspunkt}\nStartsted: {planDTO.StartSted}\nEndested: {planDTO.SlutSted}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
