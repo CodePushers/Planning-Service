bab2f79e4b14c2a0c5087af66a4a4f273be992cb
Anders <90785699+lynge96@users.noreply.github.com>
2023-03-19 17:19:45 +0100

    Fjernet try-catch og lavet kommentarer

--- ServiceWorker/Worker.cs
+++ ServiceWorker/Worker.cs
@@ -10,14 +10,12 @@ public class Worker : BackgroundService
     private readonly ILogger<Worker> _logger;
     private readonly string _filePath;
     private readonly string _hostName;
-    private readonly string _AMQP_URL;
 
     public Worker(ILogger<Worker> logger, IConfiguration config)
     {
         _logger = logger;
-
+        // Henter miljø variabel "FilePath" og "HostnameRabbit" fra docker-compose
         _filePath = config["FilePath"] ?? "/srv";
-
         _hostName = config["HostnameRabbit"];
     
         _logger.LogInformation($"Filepath: {_filePath}");
@@ -26,71 +24,65 @@ public class Worker : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        try
+
+        var factory = new ConnectionFactory
         {
-            var factory = new ConnectionFactory
+            HostName = _hostName
+        };
+        
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        // Opretter en kø "hello" hvis den ikke allerede findes i vores rabbitmq-server
+        channel.QueueDeclare(queue: "hello",
+                                durable: false,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null);
+
+        Console.WriteLine("[*] Waiting for messages.");
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        // Delegate method
+        consumer.Received += (model, ea) =>
+        {
+            // Henter data ned fra køen
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+
+            // Deserialiserer det indsendte data om til C# objekt
+            PlanDTO? plan = JsonSerializer.Deserialize<PlanDTO>(message);
+
+            Console.WriteLine($"[*] Plan modtaget:\n\tKundenavn: {plan.KundeNavn}\n\tStarttidspunkt: {plan.StartTidspunkt}\n\tStartsted: {plan.StartSted}\n\tEndested: {plan.SlutSted}");
+
+            // Tjekker om filen eksisterer og om den er tom
+            if (!File.Exists(Path.Combine(_filePath, "planListe.csv")) || new FileInfo(Path.Combine(_filePath, "planListe.csv")).Length == 0)
             {
-                // 172.17.0.2
-                HostName = _hostName
-            };
-            
-            using var connection = factory.CreateConnection();
-            using var channel = connection.CreateModel();
-
-            channel.QueueDeclare(queue: "hello",
-                                 durable: false,
-                                 exclusive: false,
-                                 autoDelete: false,
-                                 arguments: null);
-
-            Console.WriteLine(" [*] Waiting for messages.");
-
-            var consumer = new EventingBasicConsumer(channel);
-
-            // Delegate method
-            consumer.Received += (model, ea) =>
-            {
-                var body = ea.Body.ToArray();
-                var message = Encoding.UTF8.GetString(body);
-
-                PlanDTO? plan = JsonSerializer.Deserialize<PlanDTO>(message);
-
-                Console.WriteLine($"Plan modtaget:\nKundenavn: {plan.KundeNavn}\nStarttidspunkt: {plan.StartTidspunkt}\nStartsted: {plan.StartSted}\nEndested: {plan.SlutSted}");
-
-
-                // Tjekker om filen eksisterer og om den er tom
-                if (!File.Exists(Path.Combine(_filePath, "planListe.csv")) || new FileInfo(Path.Combine(_filePath, "planListe.csv")).Length == 0)
+                // Laver en ny "planListe.csv" fil på stien
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine(_filePath, "planListe.csv")))
                 {
-                    // Skriver headeren til .CSV-filen
-                    using (StreamWriter outputFile = new StreamWriter(Path.Combine(_filePath, "planListe.csv")))
-                    {
-                        outputFile.WriteLine("Kundenavn,Starttidspunkt,Startsted,Slutsted");
-                        outputFile.Close();
-                    }
-                }
-
-                // StreamWriter til at sende skrive i .CSV-filen
-                using (StreamWriter outputFile = new StreamWriter(Path.Combine(_filePath, "planListe.csv"), true))
-                {
-
-                    outputFile.WriteLineAsync($"{plan.KundeNavn},{plan.StartTidspunkt},{plan.StartSted},{plan.SlutSted}");
-
+                    // Opretter headeren i filen og lukker den
+                    outputFile.WriteLine("Kundenavn,Starttidspunkt,Startsted,Slutsted");
                     outputFile.Close();
                 }
+            }
 
-            };
+            // StreamWriter til at sende skrive i .CSV-filen
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(_filePath, "planListe.csv"), true))
+            {
+                // Laver en ny linje med det tilsendte data og lukker filen.
+                outputFile.WriteLineAsync($"{plan.KundeNavn},{plan.StartTidspunkt},{plan.StartSted},{plan.SlutSted}");
+                outputFile.Close();
+            }
 
-            channel.BasicConsume(queue: "hello",
-                                 autoAck: true,
-                                 consumer: consumer);
+        };
+
+        channel.BasicConsume(queue: "hello",
+                                autoAck: true,
+                                consumer: consumer);
 
 
-        }
-        catch (Exception ex)
-        {
-            _logger.LogInformation(ex.Message);
-        }
-        
         while (!stoppingToken.IsCancellationRequested)
         {
             _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
